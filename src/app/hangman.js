8b8f7f7edb70d72b0451c3386ba195e72a42c022
Caroline Paulic <carolinepaulic@gmail.com>
2016-09-01 20:09:13 -0800

    Alphabet selection looks a little nicer

--- src/app/hangman.js
+++ src/app/hangman.js
@@ -1,15 +1,4 @@
 angular
-  .module('hangman.game-module', [])
-  .config(function($stateProvider) {
-    $stateProvider.state('game', {
-      url: '/play',
-      templateUrl: 'modules/game/game.html',
-      controller: 'GameController',
-      controllerAs: 'ctrl'
-    });
-  });
-
-angular
   .module('hangman.welcome-module', [])
   .config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
@@ -20,6 +9,26 @@ angular
       controllerAs: 'ctrl'
     });
   });
+
+angular
+  .module('hangman.game-module', [])
+  .config(function($stateProvider) {
+    $stateProvider.state('game', {
+      url: '/play',
+      templateUrl: 'modules/game/game.html',
+      controller: 'GameController',
+      controllerAs: 'ctrl'
+    });
+  });
+
+(function() {
+  function Controller() {
+    var ctrl = this;
+  }
+
+  angular.module('hangman.welcome-module')
+    .controller('WelcomeController', [Controller]);
+})();
 
 (function() {
   function Controller($state, HangmanFigureService) {
@@ -176,15 +185,6 @@ angular
   angular
   .module('hangman.game-module')
   .directive('letterGuessBox', Directive);
-})();
-
-(function() {
-  function Controller() {
-    var ctrl = this;
-  }
-
-  angular.module('hangman.welcome-module')
-    .controller('WelcomeController', [Controller]);
 })();
 
 angular.module('hangman', [
