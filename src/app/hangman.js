b5dfda000dcb1cedff31f86dc9b98295a8feda48
Caroline Paulic <carolinepaulic@gmail.com>
2016-09-14 20:33:03 -0800

    Added basic typography rules to CSS

--- src/app/hangman.js
+++ src/app/hangman.js
@@ -1,16 +1,4 @@
 angular
-  .module('hangman.welcome-module', [])
-  .config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
-    $stateProvider.state('welcome', {
-      url: '/',
-      templateUrl: 'modules/welcome/welcome.html',
-      controller: 'WelcomeController',
-      controllerAs: 'ctrl'
-    });
-  });
-
-angular
   .module('hangman.game-module', [])
   .config(function($stateProvider) {
     $stateProvider.state('game', {
@@ -21,14 +9,17 @@ angular
     });
   });
 
-(function() {
-  function Controller() {
-    var ctrl = this;
-  }
-
-  angular.module('hangman.welcome-module')
-    .controller('WelcomeController', [Controller]);
-})();
+angular
+  .module('hangman.welcome-module', [])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider.state('welcome', {
+      url: '/',
+      templateUrl: 'modules/welcome/welcome.html',
+      controller: 'WelcomeController',
+      controllerAs: 'ctrl'
+    });
+  });
 
 (function() {
   function Controller($state, HangmanFigureService, WordService) {
@@ -198,6 +189,15 @@ angular
   angular
     .module('hangman.game-module')
     .service('WordService', ['$http', Service]);
+})();
+
+(function() {
+  function Controller() {
+    var ctrl = this;
+  }
+
+  angular.module('hangman.welcome-module')
+    .controller('WelcomeController', [Controller]);
 })();
 
 angular.module('hangman', [
